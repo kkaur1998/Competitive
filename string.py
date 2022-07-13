2d9491b084611c79b7f912442b259229f8e6b7d1
kkaur1998 <kamaljeetkaur.malik@gmail.com>
2022-07-13 15:18:49 +0530

    combine lists to create dictionary

--- string.py
+++ string.py
@@ -1,14 +1,23 @@
+
+#create reverse of a string without affecting special characters
+
 s = "a!!!b.c.d,e'f,ghi"
 k = ""
 m = ""
+
+#making a separate string "k" which will contain all alphas of string s
+# in k : replacing special characters with ' '
 for i in s:
     if i.isalpha():
         k += i
     else:
         k += ' '
 l = k.replace(' ','')
+#creating reverse of string without special characters
 l = l[::-1]
 x=0
+
+#replacing all spaces in k with the special characters of s
 for i in range(len(k)):
     if(k[i] == ' '):
         m += s[i]
